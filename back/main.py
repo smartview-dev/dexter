a2c671777ebfcec579ff5b881aa41ec0514dc189
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2025-04-19 02:01:08 -0500

    feat(cors): add CORS middleware with allowed origins for API

--- back/main.py
+++ back/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from modules.users.v1.routes import router as v1_user_router
+
+origins = [
+    "dexter.smartview.dev",
+    "http://localhost:4200",
+]
 
 
 app = FastAPI(
@@ -10,6 +17,14 @@ app = FastAPI(
         {"name": "v1", "description": "Version 1 of the API"},
         {"name": "users", "description": "User management"},
     ],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(v1_user_router)
